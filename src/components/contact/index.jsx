236036d8cefd151a39fd0ced375a1b9f48fb6845
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-17 15:15:47 -0300

    12-Contact form
    12-Contact form

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,8 +1,89 @@
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+import { Alert } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+
 
 const Contact = () => {
+    const dispatch = useDispatch();
+    const formik = useFormik({
+        initialValues:{email:'',firstname:'',lastname:'',message:''},
+        onSubmit:(values,{ resetForm })=>{
+            console.log(values)
+        }
+    })
+
+
     return(
         <>
-        <h1>Contact</h1>
+            <h1>Contact us</h1>
+            <form className="mt-3" onSubmit={formik.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        placeholder="email@example.com"
+                        {...formik.getFieldProps('email')}
+                    />
+                    { formik.errors.email && formik.touched.email ?
+                        <Alert variant="danger">
+                            {formik.errors.email}
+                        </Alert>
+                    :null}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="firstname">First name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="firstname"
+                        placeholder="Enter your name"
+                        {...formik.getFieldProps('firstname')}
+                    />
+                    { formik.errors.firstname && formik.touched.firstname ?
+                        <Alert variant="danger">
+                            {formik.errors.firstname}
+                        </Alert>
+                    :null}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="lastname">Last name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="lastname"
+                        placeholder="Enter your lastname"
+                        {...formik.getFieldProps('lastname')}
+                    />
+                    { formik.errors.lastname && formik.touched.lastname ?
+                        <Alert variant="danger">
+                            {formik.errors.lastname}
+                        </Alert>
+                    :null}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        className="form-control"
+                        name="message"
+                        rows={3}
+                        {...formik.getFieldProps('message')}
+                    />
+                    { formik.errors.message && formik.touched.message ?
+                        <Alert variant="danger">
+                            {formik.errors.message}
+                        </Alert>
+                    :null}
+                </div>
+
+                <button type="submit" className="btn btn-primary mt-2">
+                    Send message
+                </button>
+
+
+            </form>
         </>
     )
 }
