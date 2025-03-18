777debf4d5e4bb154be49bebdee42d7ad0cac632
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-18 12:39:12 -0300

    15-Newsletter form
    15-Newsletter form

---
+++ src/components/utils/newsletter.jsx
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { toast } from 'react-toastify'
+
+const NewsLetter = () => {
+    const textInput = useRef();
+    const dispatch = useDispatch();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = textInput.current.value;
+
+        if(value === ''){
+            toast.error('Empty email',{
+                position:'bottom-right'
+            });
+            return false;
+        }
+
+        // POSTING
+
+    }
+
+
+   return(
+    <div className="newsletter_container">
+        <h1>Join our newsletter</h1>
+        <div className="form">
+            <Form className="mt-4" onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Control
+                        type="email"
+                        placeholder="youremail@gmail.com"
+                        name="email"
+                        ref={textInput}
+                    />
+                </Form.Group>
+                <Button className="mt-2" variant="primary" type="submit">
+                    Add me to the list
+                </Button>
+            </Form>
+        </div>
+    </div>
+
+   )
+}
+
+export default NewsLetter;
